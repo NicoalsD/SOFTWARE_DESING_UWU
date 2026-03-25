2486c83f483c0e790adc1a0ea5c86fe10fc1be0d
NicoalsD <22334455nico@gmail.com>
2026-03-25 12:31:07 -0500

    Main para el ejercicio 2

---
+++ BLOQUE_5/Ejercicio2/Main.java
@@ -0,0 +1,28 @@
+package BLOQUE_5.Ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+
+        Circle circle = new Circle(2.5, "blue", true);
+        System.out.println(circle);
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Perimetro: " + circle.getPerimeter());
+
+        Rectangle rectangle = new Rectangle(4.0, 6.0, "green", false);
+        System.out.println(rectangle);
+        System.out.println("Area: " + rectangle.getArea());
+        System.out.println("Perimetro: " + rectangle.getPerimeter());
+
+        Square square = new Square(3.0, "yellow", true);
+        System.out.println(square);
+        System.out.println("Area: " + square.getArea());
+        System.out.println("Perimetro: " + square.getPerimeter());
+
+
+        square.setLength(5.0);
+        System.out.println("\nSquare despues de setLength(5.0): " + square);
+    }
+}
