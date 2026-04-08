c7a8bd36cf2a8031cf281f02b60fcd466a377ab1
chenife <jenifer.urbano@campusucc.edu.co>
2026-04-08 16:29:34 -0500

    clases restantes

--- Bloque7/Disco.java
+++ Bloque7/Disco.java
@@ -1,4 +1,33 @@
 package Bloque7;
 
-public class Disco {
+public class Disco extends Publicacion {
+    private float duracionMinutos;
+
+    public Disco(String titulo, double precio, float duracionMinutos) {
+        super(titulo, precio);
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public Disco(float duracionMinutos) {
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public Disco() {
+        this.duracionMinutos = 0:
+    }
+
+    public float getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public void setDuracionMinutos(float duracionMinutos) {
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    @Override
+    public String toString() {
+        return "Disco{" +
+                "duracionMinutos=" + duracionMinutos +
+                '}';
+    }
 }
