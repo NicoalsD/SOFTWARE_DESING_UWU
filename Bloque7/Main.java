bb5f851b0aed127cd27e21a61cefb447441c67da
chenife <jenifer.urbano@campusucc.edu.co>
2026-04-08 16:45:28 -0500

    milibro

--- Bloque7/Main.java
+++ Bloque7/Main.java
@@ -22,5 +22,8 @@ public class Main{
         System.out.println("cual es el numero de paginas del libro?");
         numeroPaginas = input.nextInt();
 
+        Libro miLibro=new Libro(titulo,precio,anioPublicacion,numeroPaginas);
+        System.out.println(miLibro);
+
     }
 }
